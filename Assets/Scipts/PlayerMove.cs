ec14cd7006984f90bbd33467ad2ddd68d805991c
Asmondia <maddisonben@live.com>
2022-02-07 17:54:00 +0000

    Movement and Tilesets created
    Movement and Tilesets created

--- Assets/Scipts/PlayerMove.cs
+++ Assets/Scipts/PlayerMove.cs
@@ -8,30 +8,71 @@ public class PlayerMove : MonoBehaviour
     private float mySpeed;
     private Rigidbody2D rb2D;
     private Transform myTransform;
+    private Vector2Int targetSpace;
+    private Vector2Int previousSpace;
     // Start is called before the first frame update
     private void Awake()
     {
-        mySpeed = setSpeed / 100.0f;
+
+        mySpeed = setSpeed;
         rb2D = GetComponent<Rigidbody2D>();
         myTransform = transform;
+        targetSpace = new Vector2Int(Mathf.RoundToInt(myTransform.position.x), Mathf.RoundToInt(myTransform.position.y));
+        previousSpace = targetSpace;
+        
     }
 
     // Update is called once per frame
     private void Update()
     {
-        Move();
+        var moving = (Vector2)myTransform.position != targetSpace;
+        if (moving)
+        {
+            MoveTowardsTargetSpace();
+        }
+        else
+        {
+            previousSpace = targetSpace;
+            SetTargetSpace();
+        }
+        
 
     }
-    private void Move()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (Input.GetKey(KeyCode.W))
-            rb2D.position += Vector2.up * mySpeed;
-        if (Input.GetKey(KeyCode.S))
-            rb2D.position += Vector2.down * mySpeed;
-        if (Input.GetKey(KeyCode.A))
-            rb2D.position += Vector2.left * mySpeed;
-        if (Input.GetKey(KeyCode.D))
-            rb2D.position += Vector2.right * mySpeed;
-
+        Debug.Log("Help");
+        targetSpace = previousSpace;
     }
+    private void MoveTowardsTargetSpace()
+    {
+        myTransform.position = Vector2.MoveTowards(myTransform.position, targetSpace, mySpeed * Time.deltaTime);
+    }
+    private void SetTargetSpace()
+    {
+        if(Input.GetKey(KeyCode.W) ^ Input.GetKey(KeyCode.S))
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                targetSpace += Vector2Int.up;
+            }
+
+            else if (Input.GetKey(KeyCode.S))
+            {
+                targetSpace += Vector2Int.down;
+            }
+        }
+   
+        else if (Input.GetKey(KeyCode.A) ^ Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                targetSpace += Vector2Int.left;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                targetSpace += Vector2Int.right;
+            }
+        }
+    }
+   
 }
